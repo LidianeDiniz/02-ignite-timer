acb6da35115d0d26d1dd5f619bd8afb6795f1f72
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-02 15:53:45 -0300

    TypingThemes/GlobalStyles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { GlobalStyle } from "./components/global";
 import { defaultTheme } from "./styles/themes/default";
 export function App() {
   return (
@@ -9,6 +10,8 @@ export function App() {
         <Button variant="secondary" />
         <Button variant="danger" />
         <Button variant="success" />
+
+        <GlobalStyle/>
       </ThemeProvider>
   
   );
