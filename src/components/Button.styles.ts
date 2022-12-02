6eb90cd2cda641f7957f208e597f5d2a7af7f88b
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-01 22:57:52 -0300

    Config-Themes

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -19,11 +19,14 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width:100px;
     height:40px;
 
-    ${props => {
+    background-color: ${props => props.theme.primary};
+    color: ${props => props.theme.white};
+
+    /*${props => {
         return css
         `background-color: ${buttonVariants[props.variant]}`
         
-    }}
+    }}*/
  
     
 `;
