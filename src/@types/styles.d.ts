074595f97c7f33338354818f8412a6c7804e5c1c
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-03 18:27:41 -0300

    Eslint

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,8 +1,8 @@
-/* eslint-disable prettier/prettier */
 import 'styled-components'
 import { defaultTheme } from '../styles/themes/default'
 
 type ThemeType = typeof defaultTheme
+
 declare module 'styled-components' {
   export interface DefaultTheme extends ThemeType {}
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button'
-import { GlobalStyle } from './components/global'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
