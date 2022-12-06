c15c22741a2ffd3b832a3bea6f1f53f7e1a39417
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-06 16:32:36 -0300

    Page:History

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,66 @@
+import { HistoryConatiner, HistoryList } from "./styles";
+
+export function History() {
+  return (
+    <HistoryConatiner>
+      <h1>Meu hitórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryConatiner>
+  );
+}
