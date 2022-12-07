43d9cb2a481f1d2b03ab4f77ed716194522fe7e7
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-07 17:49:42 -0300

    Validating Forms

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import { Play } from "phosphor-react";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import {
   HomeConatiner,
   CountdownContainer,
@@ -25,8 +27,18 @@ function register(name: string){
   }
 */
 
+const newCycleFormValidationsSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo deve ser de no minímo 5 minutos")
+    .max(60, "O ciclo deve ser de no minímo 60 minutos"),
+});
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm();
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationsSchema),
+  });
 
   function handleCreateNewCycle(data: any) {
     console.log(data);
