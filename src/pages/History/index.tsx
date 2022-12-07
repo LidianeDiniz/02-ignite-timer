db7ee9e4c6e526ed86283d3ec526afbd8ff2e3e2
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-12-06 21:54:04 -0300

    Components: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryConatiner, HistoryList } from "./styles";
+import { HistoryConatiner, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -21,42 +21,54 @@ export function History() {
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Hà 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor={"green"}>Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Hà 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor={"green"}>Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Hà 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor={"green"}>Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Hà 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor={"green"}>Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Hà 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor={"yellow"}>Em andamento</Status>
+              </td>
             </tr>
 
             <tr>
               <td>Tarefa</td>
               <td>20 minutos</td>
               <td>Hà 2 meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor={"red"}>Interrompido</Status>
+              </td>
             </tr>
           </tbody>
         </table>
